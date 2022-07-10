d7f48aafdd46c9667a270617222100eaed05f57b
atnbn <86778371+atnbn@users.noreply.github.com>
2022-07-10 13:03:51 +0200

    implementing class

--- src/app/restaurants/restaurants.component.ts
+++ src/app/restaurants/restaurants.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { restaurants } from 'src/module/restaurants.class';
 
 @Component({
   selector: 'app-restaurants',
@@ -7,8 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RestaurantsComponent implements OnInit {
 
-
-local:string ="Döner Murat"; 
+  restaurant = new restaurants();
 
 
 
---
+++ src/module/restaurants.class.ts
@@ -0,0 +1,16 @@
+export class restaurants {
+
+
+
+    name = ["Döner Murat" , "Pizza Mario" , "Hamburger Kök"]; 
+
+    img =  ["assets/images/döner.jpg" , "assets/images/pizza.jpg" , "assets/images/hamburger.jpg"];
+    
+
+
+
+
+
+
+
+}
