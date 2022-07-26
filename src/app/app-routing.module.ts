89d0c6cf5291b866b529daa884d9bfce9b4b970e
atnbn <86778371+atnbn@users.noreply.github.com>
2022-07-26 23:20:53 +0200

    add

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MenuComponent } from './menu/menu.component';
+import { TurkishComponent } from './turkish/turkish.component';
 
 const routes: Routes = [
-  { path: 'menu' , component: MenuComponent}
+  { path: 'menu', component: MenuComponent },
+  { path: 'turkish', component: TurkishComponent },
 ];
 
 @NgModule({
